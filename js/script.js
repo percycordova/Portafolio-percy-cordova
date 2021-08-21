b30b21cd98ffe312b92b1b56bbfd116375e3bb92
percycordova <cordova_inf@hotmail.com>
2021-08-21 03:21:20 -0500

    pokemon v1

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@
                 //lef:0
             });
         }
-        
+
     });
 })(document);
 /***********************Modal***************************/
@@ -76,54 +76,69 @@
     const $portfolios = d.querySelectorAll(".portfolio-grid a"),      //Recibo todos las etiquetas a que esten dentro de mi .grid-portfolio
         $trabajos = d.querySelectorAll("article.modal .modal-close"), //Recibo todos los div con la clase .modal-close que esten dentro de article.modal  
         $contentModal = d.querySelector(".modal-content"); //Recibo la div del contenedor del modall
+    const $modalesTRabajos = d.querySelectorAll("#modales-proyectos article");
 
-    $portfolios.forEach((el, acc) => { 
-        el.addEventListener("click", (e) => {
-            d.querySelector(`#trabajo-${acc + 1}`).classList.add("open-modal");//Dependiendo del click del enlace me abre mi modal
-            $contentModal.classList.remove("close-modal-close");
-        });
+    $portfolios.forEach(el => {
 
-    });
-    $trabajos.forEach((el, acc) => {
         el.addEventListener("click", () => {
-            $contentModal.classList.add("close-modal-close");
-            d.querySelector(`#trabajo-${acc + 1}`).classList.remove("open-modal");
+
+            $modalesTRabajos.forEach(tra => {
+
+                if (`#${tra.id}` === (el.attributes[0].value)) {
+                    d.querySelector(`#${tra.id}`).classList.add("open-modal");
+                    d.querySelector(`#${tra.id} .modal-content`).classList.remove("close-modal-close");
+                }
+            });
         });
     });
+
+    $modalesTRabajos.forEach(el => {
+        (el.querySelector("div a")).addEventListener('click', () => {
+            d.querySelector(`#${el.id} .modal-content`).classList.add("close-modal-close");
+            d.querySelector(`#${el.id}`).classList.remove("open-modal");
+        })
+    });
+
+    /* $trabajos.forEach((el, acc) => {
+         el.addEventListener("click", () => {
+             $contentModal.classList.add("close-modal-close");
+             d.querySelector(`#trabajo-${acc + 1}`).classList.remove("open-modal");
+         });
+     });*/
 })(document);
 
 /* ********** ContactForm ********** */
 ((d) => {
     const $form = d.querySelector(".contact-form"),
-      $loader = d.querySelector(".contact-form-loader"),
-      $response = d.querySelector(".contact-form-response");
-  
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-form-response");
+
     $form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      $loader.classList.remove("none");
-      fetch("https://formsubmit.co/ajax/cordovaflores1994@hotmail.com", {
-        method: "POST",
-        body: new FormData(e.target),
-      })
-        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-        .then((json) => {
-          console.log(json);
-          location.hash = "#gracias";
-          $form.reset();
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/cordovaflores1994@hotmail.com", {
+            method: "POST",
+            body: new FormData(e.target),
         })
-        .catch((err) => {
-          console.log(err);
-          let message =
-            err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
-          $response.querySelector(
-            "h3"
-          ).innerHTML = `Error ${err.status}: ${message}`;
-        })
-        .finally(() => {
-          $loader.classList.add("none");
-          setTimeout(() => {
-            location.hash = "#close";
-          }, 3000);
-        });
+            .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+            .then((json) => {
+                console.log(json);
+                location.hash = "#gracias";
+                $form.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+                let message =
+                    err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+                $response.querySelector(
+                    "h3"
+                ).innerHTML = `Error ${err.status}: ${message}`;
+            })
+            .finally(() => {
+                $loader.classList.add("none");
+                setTimeout(() => {
+                    location.hash = "#close";
+                }, 3000);
+            });
     });
-  })(document);
+})(document);
